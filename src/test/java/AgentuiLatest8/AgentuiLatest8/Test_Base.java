0bd00dfe1a65a0c4f816bcc583fec2314ceb9576
HimanshuVarun <52037470+HimanshuVarun@users.noreply.github.com>
2020-01-30 13:18:37 +0530

    Update Test_Base.java

--- src/test/java/AgentuiLatest8/AgentuiLatest8/Test_Base.java
+++ src/test/java/AgentuiLatest8/AgentuiLatest8/Test_Base.java
@@ -22,6 +22,7 @@ public class Test_Base extends Home_page
 		extent.addSystemInfo("Environment", "Automation Testing");
 		extent.addSystemInfo("User Name", "Himanshu");
 		Home_page.openBrowser();
+		Data();
 		logger = extent.startTest("openbrowser");
 		//Assert.assertTrue(true);
 		logger.log(LogStatus.PASS, "browser test case is pass");
